ebe2fe99bd942cf85834fc6fe09f17a4b41a8f88
pianomister <5443951+pianomister@users.noreply.github.com>
2019-07-07 01:25:32 +0200

    Publish tool in docs folder

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -8,7 +8,7 @@ module.exports = {
     app: Path.resolve(__dirname, '../src/scripts/index.js')
   },
   output: {
-    path: Path.join(__dirname, '../build'),
+    path: Path.join(__dirname, '../docs'),
     filename: 'js/[name].js'
   },
   optimization: {
