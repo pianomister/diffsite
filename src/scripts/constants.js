aad621099cad4edb424e8d68d6f4dadf028e4264
pianomister <5443951+pianomister@users.noreply.github.com>
2019-08-02 15:16:35 +0200

    Publish version 1.1.1
    Changes in this version:
    - support for localhost URLs
    - add "getamp=1" parameter in URL to auto-detect AMP page
    - improve layout and styling

---
+++ src/scripts/constants.js
@@ -0,0 +1,47 @@
+export const DEVICES = [
+    {
+        name: 'iPhone 5/SE',
+        width: 320,
+        height: 568
+    },
+    {
+        name: 'Galaxy S5',
+        width: 360,
+        height: 640
+    },
+    {
+        name: 'iPhone 6/7/8',
+        width: 375,
+        height: 667
+    },
+    {
+        name: 'iPhone X/XS',
+        width: 375,
+        height: 812
+    },
+    {
+        name: 'Pixel 2',
+        width: 411,
+        height: 731
+    },
+    {
+        name: 'Pixel 2 XL',
+        width: 411,
+        height: 823
+    },
+    {
+        name: 'iPhone 6/7/8 Plus',
+        width: 414,
+        height: 736
+    },
+    {
+        name: 'iPad',
+        width: 768,
+        height: 1024
+    },
+    {
+        name: 'iPad Pro',
+        width: 1024,
+        height: 1366
+    }
+];
